06595c863c7b90d5d0b7ad6295d4492efb047c9f
Emiliano Bovetti <emiliano.bovetti@prima.it>
2022-08-04 16:34:18 +0200

    chore: bump to elixit 1.9

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule GenRetry.Mixfile do
       description: description(),
       package: package(),
       version: "1.4.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.9",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       test_coverage: [tool: ExCoveralls],
